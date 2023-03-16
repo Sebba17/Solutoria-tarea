0be015f180cbffd65e5907d7214f8730cdaae5e1
Sebastian Mayanes <Sebastian.mayanes@gmail.com>
2023-03-15 22:47:03 -0300

    Borrado archivo equivocado

--- conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-//conexion para subir.php
-$mysqli = new mysqli("localhost", "root", "", "solutoria-tarea");
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-?>
-
